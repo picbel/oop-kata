90d847e1d8f3d874c223ab328318c6a453f6b76a
Picbel <picbel.gon@gmail.com>
2022-02-08 22:29:14 +0900

    Initial commit

---
+++ src/main/java/com/study/oopkata/OopKataApplication.java
@@ -0,0 +1,13 @@
+package com.study.oopkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopKataApplication.class, args);
+    }
+
+}
